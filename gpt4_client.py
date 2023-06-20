6627e476ea2ea11f3c7a8f664c115450f0a99c08
Anil Tolwani <anil@unlearn.ai>
2023-06-19 17:19:21 -0700

    refactor to obfuscate the system message less

--- gpt4_client.py
+++ gpt4_client.py
@@ -5,6 +5,7 @@ import streamlit as st
 
 MODEL = "gpt-4"
 
+
 class GPT4Client:
     def __init__(self):
         ## get api key from os environ ['OPENAI_API_KEY']
@@ -15,44 +16,36 @@ class GPT4Client:
             openai.api_key = st.secrets['OPENAI_API_KEY']
 
 
-    def generate_patient_history(self, conversation: str) -> str:
+    def call_openai_chat(system_message: str, user_message: str) -> str:
 
         response = openai.ChatCompletion.create(
             model=MODEL,
             messages=[
-                {"role": "system", "content": "Generate a medical history for this patient in SOAP Format based on the following conversation between a vet and a client."},
-                {"role": "user", "content": conversation},
+                {"role": "system", "content": system_message},
+                {"role": "user", "content": user_message},
             ],
             temperature=1,
         )
 
-        patient_output = response['choices'][0]['message']['content']
-        return patient_output
+        return response['choices'][0]['message']['content']
+
+    def generate_patient_history(self, conversation: str) -> str:
+        
+        system_message = "Generate a medical history for this patient in SOAP Format based on the following conversation between a vet and a client."
+        user_message = conversation
+
+        return self.call_openai_chat(system_message, user_message)
 
     def generate_diagnoses(self, conversation: str) -> List[str]:
+        
+        system_message = "Generate a list of potential diagnoses based on the following conversation between a vet and a client. Format it so that each diagnoses is separated by a newline, and don't include any other information."
+        user_message = conversation
 
-        response = openai.ChatCompletion.create(
-            model=MODEL,
-            messages=[
-                {"role": "system", "content": "Generate a list of potential diagnoses based on the following conversation between a vet and a client. Format it so that each diagnoses is separated by a newline, and don't include any other information."},
-                {"role": "user", "content": conversation},
-            ],
-            temperature=1,
-        )
-
-        diagnoses = response['choices'][0]['message']['content'].split("\n")
-        return diagnoses
+        return self.call_openai_chat(system_message, user_message).split("\n")
 
     def generate_treatment(self, diagnosis: str, conversation: str) -> str:
+        
+        system_message = "Generate a treatment plan based on the diagnosis and the following conversation between a vet and a client."
+        user_message = f"Diagnosis: {diagnosis};;; Conversation: {conversation}"
 
-        response = openai.ChatCompletion.create(
-            model=MODEL,
-            messages=[
-                {"role": "system", "content": "Generate a treatment plan based on the diagnosis and the following conversation between a vet and a client."},
-                {"role": "user", "content": f"Diagnosis: {diagnosis};;; Conversation: {conversation}"},
-            ],
-            temperature=1,
-        )
-
-        diagnoses = response['choices'][0]['message']['content']
-        return diagnoses
+        return self.call_openai_chat(system_message, user_message)
