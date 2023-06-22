c78d3817299330e49445b2aecbb76109c305a1f1
rtolwani <37891906+rtolwani@users.noreply.github.com>
2023-06-22 12:47:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from gpt4_client import GPT4Client
 def main():
     def update_diagnosis():
         with col3:
-            diagnosis_name = st.selectbox("Select a Differential from the List Below:", st.session_state['diagnoses'])
+            diagnosis_name = st.selectbox("Choose a Differential for Treatment Plan. Change Selection for Another Plan:", st.session_state['diagnoses'])
             st.session_state['diagnosis_name'] = diagnosis_name
                 
             gpt4_client = st.session_state['gpt4_client']
             diagnosis_name = st.session_state['diagnosis_name']
             conversation = st.session_state['conversation']
 
-            if st.button("Get Treatment Plan Based on Diagnosis"):
+            if st.button("Get Expanded Treatment Plan"):
                 st.markdown(f"**Diagnosis:**\n\n{st.session_state['diagnosis_name']}")  # Use markdown for better formatting
 
                 treatment = gpt4_client.generate_treatment(diagnosis_name, conversation)
@@ -21,7 +21,7 @@ def main():
     col1, col2, col3 = st.columns([3, 1, 3]) # Create two columns
 
     with col1:
-        conversation = st.text_area("Enter Patient History", height=450)  # Increase textarea height
+        conversation = st.text_area("Enter Patient History. Modify to Update SOAP and Differentials", height=450)  # Increase textarea height
         if st.button("Generate Patient SOAP"):
             gpt4_client = GPT4Client()
             diagnoses = gpt4_client.generate_diagnoses(conversation)
