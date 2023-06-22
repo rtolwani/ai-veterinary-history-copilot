d45400d2273d01075e53421e2f1fec7dd4c9642b
rtolwani <37891906+rtolwani@users.noreply.github.com>
2023-06-22 13:09:20 -0400

    Update gpt4_client.py

--- gpt4_client.py
+++ gpt4_client.py
@@ -31,21 +31,21 @@ class GPT4Client:
 
     def generate_patient_history(self, conversation: str) -> str:
         
-        system_message = "Generate a medical history for this patient in SOAP Format based on the following conversation between a vet and a client. It should be formatted so that each section is separated by a newline, and the title of each section is followed by a colon. Don't include any other information."
+        system_message = "Generate a medical history for this patient in SOAP Format based on the pet's patient history. It should be formatted so that each section is separated by a newline, and the title of each section is followed by a colon. Don't include any other information."
         user_message = conversation
 
         return self.call_openai_chat(system_message, user_message)
 
     def generate_diagnoses(self, conversation: str) -> List[str]:
         
-        system_message = "Generate a list of potential diagnoses based on the following conversation between a vet and a client. Format it so that each diagnoses is separated by a newline, and don't include any other information."
+        system_message = "Generate a list of potential diagnoses based on the pet's patient history. Format it so that each diagnoses is separated by a newline, and don't include any other information."
         user_message = conversation
 
         return self.call_openai_chat(system_message, user_message).split("\n")
 
     def generate_treatment(self, diagnosis: str, conversation: str) -> str:
         
-        system_message = "Generate a treatment plan based on the diagnosis and the following conversation between a vet and a client."
+        system_message = "Generate a treatment plan based on the diagnosis based on the pet's patient history."
         user_message = f"Diagnosis: {diagnosis};;; Conversation: {conversation}"
 
         return self.call_openai_chat(system_message, user_message)
