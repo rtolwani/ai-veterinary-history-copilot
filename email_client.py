c366cf78a4afd00945ed879452f525ffb00b839e
Anil Tolwani <anil@unlearn.ai>
2023-06-22 22:38:18 -0700

    api key

--- email_client.py
+++ email_client.py
@@ -1,9 +1,15 @@
 import sendgrid
 from sendgrid.helpers.mail import Email, To, Content, Mail
 import os
+import streamlit as st
 
 def send_email(email, subject, content):
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    if os.environ.get('SEMDGRID_API_KEY') is not None:
+        sendgrid_key = os.environ.get('SENDGRID_API_KEY')
+    else:
+        sendgrid_key = st.secrets['SENDGRID_API_KEY']
+
+    sg = sendgrid.SendGridAPIClient(api_key=sendgrid_key)
     from_email = Email("ravi@tolwani.com")  # Replace with your SendGrid account email
     to_email = To(email)
     content = Content("text/plain", content)
