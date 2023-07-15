990cf2fb0b0193746345f8af4e684f4383f1cdaf
rtolwani <37891906+rtolwani@users.noreply.github.com>
2023-07-14 21:53:37 -0400

    Update app.py
    rearranged steps

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ def main():
             st.session_state['pdf_string'] = pdf_string 
             os.unlink(fp.name)
 
-        conversation = st.text_area("Step 2: (Optional) Enter Patient Singlement and History. Modify to Update Differentials and Medical Plan.", height=100)  # Increase textarea height
+        conversation = st.text_area("(Optional) Enter Patient Singlement and History. Modify to Update Differentials and Medical Plan.", height=100)  # Increase textarea height
 
-        if st.button("Step 3: Generate Potential Differentials"):
+        if st.button("Step 2: Generate Potential Differentials"):
             gpt4_client = GPT4Client()
 
             # ## add a loading spinner
@@ -68,13 +68,13 @@ def main():
             ## get just the diagnoses from the JSON
             diagnoses_no_justification = [diag['diagnosis'] for diag in st.session_state['diagnoses']]
 
-            diagnosis_name = st.multiselect("Step 4: Select One or More Differential(s). Revise to Modify Medical Record.", diagnoses_no_justification)
+            diagnosis_name = st.multiselect("Step 3: Select One or More Differential(s). Revise to Modify Medical Record.", diagnoses_no_justification)
             st.session_state['chosen_diagnoses'] = diagnosis_name
                 
             gpt4_client = st.session_state['gpt4_client']
             conversation = st.session_state['conversation']
 
-            if st.button("Step 5: Get Treatment Plan and Medical Summary with your Selected Differential(s)"):
+            if st.button("Step 4: Get Treatment Plan and Medical Summary with your Selected Differential(s)"):
                 st.markdown("**Potential Diagnoses:**\n\n")  # Use markdown for better formatting
                 for i, diagnosis in enumerate(diagnosis_name):
                     st.markdown(f"{i+1}. {diagnosis}")
